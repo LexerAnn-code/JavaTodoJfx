d7297b0c94c8776c499423d296e7b5fac339d88b
Lexer-0 <45894853+lexer-0@users.noreply.github.com>
2020-05-08 12:38:04 +0000

    Added Todo Class

---
+++ src/sample/Model/Todo.java
@@ -0,0 +1,13 @@
+package sample.Model;
+
+public class Todo {
+    String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
